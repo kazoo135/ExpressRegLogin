9fba270abd38d510d19dc494f61390b9cd02ae50
Mark Caruso <kazoo6723@gmail.com>
2018-01-21 16:10:41 -0500

    required dotenv, added properties to mysql connection, testing post data on register page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express(); 
 var mysql = require('mysql');
-
+var dotenv = require('dotenv');
+var bodyParser = require('body-parser');
 
 //set variables
 app.set('view engine', 'ejs');
@@ -10,11 +11,14 @@ app.set('port', process.env.PORT || 3000);
 
 //use packages and functions
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 
 // use routes
 app.use(require('./routes/index'));
 app.use(require('./routes/register'));
+
 
 app.listen( app.get('port'), function(){
     console.log("listening on port 3000")
--- routes/register.js
+++ routes/register.js
@@ -9,4 +9,15 @@ router.get('/register', function(req, res){
     }); 
 })
 
+router.post('/register', function(req, res){
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    console.log(username);
+    console.log(email);
+    console.log(password);
+
+ var dbConnect = require('../db.js');
+})
+
 module.exports = router;
