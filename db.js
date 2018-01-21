41aa352c4f4fc86a521125ab41ce72e57a168964
Mark Caruso <kazoo6723@gmail.com>
2018-01-21 18:36:36 -0500

    added header.ejs to index, commented out connection in db.js and am connecting on register post request

--- db.js
+++ db.js
@@ -10,9 +10,9 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+//   });
 
 module.exports = connection; 
