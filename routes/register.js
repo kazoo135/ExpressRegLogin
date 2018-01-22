a4fb9c19b40173446b66a76e7d3cc11ccddb6728
Mark Caruso <kazoo6723@gmail.com>
2018-01-21 19:42:14 -0500

    Inserting username, game_id, email, password and generating random game_id for testing

--- routes/register.js
+++ routes/register.js
@@ -9,26 +9,40 @@ router.get('/register', function(req, res){
     }); 
 })
 
+//testing function for eventual game identification number
+var gameId = function(){
+    var randId = Math.floor(Math.random()* 10000) + 1;
+    console.log(randId);
+    return randId 
+}
 router.post('/register', function(req, res){
     var username = req.body.username;
     var email = req.body.email;
     var password = req.body.password;
+    var game_id = gameId(); 
     console.log(username);
     console.log(email);
     console.log(password);
 
  var db = require('../db.js');
 
- db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+ db.query('INSERT INTO users (game_id,username,email,password) VALUES (?,?, ?,?)', 
+ [game_id, username, email, password],
+  function (error, results, fields) {
+    if (error){
+        console.log(error.message);
+        console.log(error.sqlState);
+        throw error;
+    }else{
+        res.render('register',{
+            projectTitle: 'Reiser Muzic',
+            pageTitle:'Registration complete',
+            pageId: 'regcomplete'
+        })
+    } 
+   
   });
 
-  res.render('register',{
-      projectTitle: 'Reiser Muzic',
-      pageTitle:'Registration complete',
-      pageId: 'regcomplete'
-  })
 })
 
 module.exports = router;
